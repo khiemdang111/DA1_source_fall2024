8bc4413b3bcf08417881a3352a550df25cecf240
ngtandat1212 <datntpc08679@gmail.com>
2024-11-07 22:13:08 +0700

    [client] Get all product and category page product

--- App/Models/Product.php
+++ App/Models/Product.php
@@ -78,22 +78,22 @@ class Product extends BaseModel
     //         return $result;
     //     }
     // }
-    // public function getOneProductByStatus(int $id)
-    // {
-    //     $result = [];
-    //     try {
-    //         $sql = "SELECT products.*, categories.name AS category_name FROM products INNER JOIN categories ON products.category_id = categories.id WHERE products.status=" . self::STATUS_ENABLE . " AND categories.status=" . self::STATUS_ENABLE . " AND products.id=?";
-    //         $conn = $this->_conn->MySQLi();
-    //         $stmt = $conn->prepare($sql);
+    public function getOneProductByStatus(int $id)
+    {
+        $result = [];
+        try {
+            $sql = "SELECT products.*, categories.name AS category_name FROM products INNER JOIN categories ON products.category_id = categories.id WHERE products.status=" . self::STATUS_ENABLE . " AND categories.status=" . self::STATUS_ENABLE . " AND products.id=?";
+            $conn = $this->_conn->MySQLi();
+            $stmt = $conn->prepare($sql);
 
-    //         $stmt->bind_param('i', $id);
-    //         $stmt->execute();
-    //         return $stmt->get_result()->fetch_assoc();
-    //     } catch (\Throwable $th) {
-    //         error_log('Lỗi khi hiển thị chi tiết dữ liệu: ' . $th->getMessage());
-    //         return $result;
-    //     }
-    // }
+            $stmt->bind_param('i', $id);
+            $stmt->execute();
+            return $stmt->get_result()->fetch_assoc();
+        } catch (\Throwable $th) {
+            error_log('Lỗi khi hiển thị chi tiết dữ liệu: ' . $th->getMessage());
+            return $result;
+        }
+    }
     // public function countTotalProduct(){
     //     return $this->countTotal();
     // }
