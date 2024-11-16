06faeba9fd7a00634cc924da22ea4309f186e809
khiemdang111 <dangkhiemct111@gmail.com>
2024-11-16 21:14:59 +0700

    [admin] form add product variant

--- App/Controllers/Admin/ProductController.php
+++ App/Controllers/Admin/ProductController.php
@@ -12,8 +12,6 @@ use App\Views\Admin\Components\Notification;
 use App\Views\Admin\Pages\Product\Create;
 use App\Views\Admin\Pages\Product\Edit;
 use App\Views\Admin\Pages\Product\Index;
-use App\Views\Admin\Pages\Product\SettingVariant;
-use App\Views\Admin\Pages\Product\createAttributeVariant;
 use App\Views\Admin\Pages\Recycle\ProductRecycle;
 
 class ProductController
@@ -250,75 +248,5 @@ class ProductController
             NotificationHelper::error('deletePermanently_product', 'Xóa vĩnh viễn sản phẩm thất bại!');
             header("Location: /admin/recycle/products");
         }
-    }
-    public static function createVariant($id)
-    {
-        // validation các trường dữ liệu
-        $products = new product();
-        $product = $products->getOneProductByCategoryDetailStatus($id);
-        $variant = $products->getAllProductByVariant();
-        $variant_opt = $products->getAllVariantOptionsById($id);
-        $data = [
-            'product' => $product,
-            'variant' => $variant,
-            'variant_opt' => $variant_opt,
-        ];
-        // echo '<pre>';
-        // var_dump($data['variant_opt']);
-        // die;
-        Header::render();
-        Notification::render();
-        //hủy thông báo
-        NotificationHelper::unset();
-        SettingVariant::render($data);
-        Footer::render();
-    }
-    public static function storeVariant()
-    {
-        // validation các trường dữ liệu
-        $is_valid = ProductValidation::createVariant();
-
-        if (!$is_valid) {
-            NotificationHelper::error('store_product', 'Thêm sản phẩm thất bại');
-            header('location: /admin/products');
-            exit;
-        }
-
-        // Lấy giá trị từ $_POST
-        $option_values = $_POST['option_vl_name'] ?? [];
-        $id_product = $_POST['id'];
-        $option_ids = $_POST['option_vl_name'] ?? [];
-        $pro_variant_ids = $_POST['pro_variant_id'] ?? [];
-        // Tạo mảng kết hợp
-
-        $combinedOptions = [];
-
-        foreach ($option_ids as $index => $id) {
-            if (isset($option_values[$index])) {
-                // Lấy pro_variant_id từ mảng tương ứng
-                $pro_variant_id = isset($pro_variant_ids[$index]) ? $pro_variant_ids[$index] : null;
-
-                // Thêm phần tử vào mảng kết hợp
-                $combinedOptions[] = [
-                    'product_id' => $id_product,
-                    'pro_variant_id' => $pro_variant_id,
-                    'option_id' => $id,
-                ];
-            }
-        }
-
-        $produtc = new Product();
-        $result = $produtc->createProductVariant($combinedOptions);
-    }
-
-    public function createAttributeVariant()
-    {
-
-        Header::render();
-        // hiển thị form thêm
-        Notification::render();
-        NotificationHelper::unset();
-        createAttributeVariant::render();
-        Footer::render();
     }
 }
