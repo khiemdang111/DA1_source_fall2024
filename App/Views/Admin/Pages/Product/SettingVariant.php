cf71b2afdea1f1d739d099393ef21f385421b9f7
trinhhaidao2004 <daothpc08990@gmail.com>
2024-11-16 18:24:27 +0700

    [Client] order_mails

--- App/Views/Admin/Pages/Product/SettingVariant.php
+++ App/Views/Admin/Pages/Product/SettingVariant.php
@@ -26,11 +26,7 @@ class SettingVariant extends BaseView
                 <form action="/admin/productvariant" id="" method="post" enctype="multipart/form-data">
                   <input type="hidden" name="method" id="" value="POST">
                   <div class="row g-6">
-<<<<<<< HEAD
                     <input type="hidden" name="id" value="<?= $data['product'][0]['id'] ?>">
-=======
-                    <input type="hidden" name="id" value="<?= $data['product'][0]['id']?>">
->>>>>>> b77ae48 ([admin] form add product variant)
                     <div class="col-md-12">
                       <img src="<?= APP_URL ?>/public/uploads/products/<?= $data['product'][0]['image'] ?>" width="200px"
                         class="rounded mx-auto d-block" alt="">
@@ -46,7 +42,6 @@ class SettingVariant extends BaseView
                       <input value="<?= $data['product'][0]['category_name'] ?>" type="category_name" class="form-control"
                         id="category_name" name="category_name" placeholder="Address" />
                     </div>
-<<<<<<< HEAD
                     <table class="table table-striped">
                       <div class="row mt-3 mb-3 justify-content-between">
                         <a href="javascript:void(0)" onclick="create()" class="btn btn-primary btn-sm"
@@ -119,47 +114,6 @@ class SettingVariant extends BaseView
                         </tr>
                       </tbody>
                     </table>
-=======
-                    <div class="form-group">
-                      <label for="">Thuộc tính</label>
-                      <a href="javascript:void(0)" onclick="create()" class="btn btn-primary btn-sm ">ADD</a>
-                      <div id="multi_properties">
-                        <div class="row items_properties row align-items-center">
-                          <div class="col-3 mt-2">
-                            <label for="">Tên thuộc tính</label>
-                            <select name="product_variant_name[]" id="" class="form-select">
-                              <option value="">Chọn thuộc tính</option>
-                              <?php
-                              foreach ($data['variant'] as $itemVariant):
-                                ?>
-                                <option value="<?= $itemVariant['product_variant_id'] ?>">
-                                  <?= $itemVariant['product_variant_name'] ?>
-                                </option>
-                                <?php
-                              endforeach;
-                              ?>
-                              </option>
-                            </select>
-                          </div>
-                          <div class="col-3  mt-2">
-                            <label for="option_vl_name">Giá trị</label>
-                            <input type="text" class="form-control" id="option_vl_name" name="option_vl_name[]"
-                              placeholder="Giá trị">
-                          </div>
-                          <div class="col-3  mt-2">
-                            <label for="option_vl_price">Tiền</label>
-                            <input type="text" class="form-control" id="option_vl_price" name="option_vl_price[]"
-                              placeholder="Giá trị">
-                          </div>
-                          <div class="col-1">
-                            <label for="">&nbsp;</label>
-                            <a href="javascript:void(0)" onclick="delete_(this)"
-                              class="btn btn-danger btn-sm d-block">Xóa</a>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
->>>>>>> b77ae48 ([admin] form add product variant)
                   </div>
                   <div class="mt-6">
                     <button type="submit" class="btn btn-primary me-3" name>Lưu</button>
@@ -180,7 +134,6 @@ class SettingVariant extends BaseView
       function create() {
         let count_items = document.querySelectorAll(".items_properties").length - 1;
         count_items++;
-<<<<<<< HEAD
 
         $("#multi_properties").append(`
                                                                                                         <tr class="items_properties">
@@ -227,39 +180,6 @@ class SettingVariant extends BaseView
                                               class="btn btn-danger btn-sm d-block w-50">Xóa</a>
                                           </td>
                                         </tr>                                                     `);
-=======
-        $("#multi_properties").append(`
-                                                    <div class="row items_properties mt-3">
-                                                        <div class="col-3">
-                                                            <label for="">Tên thuộc tính</label>
-                                                            <select name="product_variant_name" id="" class="form-select">
-                                                                <option value="">Chọn thuộc tính</option>
-                                                                <?php
-                                                                foreach ($data['variant_data'] as $itemVariant):
-                                                                  ?>
-                                            <option value="<?= $itemVariant['product_variant_id'] ?>">
-                                              <?= $itemVariant['product_variant_name'] ?>
-                                            </option>
-                                            <?php
-                                                                endforeach;
-                                                                ?>
-                                                            </select>
-                                                        </div>
-                                                        <div class="col-3">
-                                                            <label for="option_vl_name">Giá trị</label>
-                                                            <input type="text" class="form-control" id="option_vl_name" name="option_vl_name[]" placeholder="Giá trị">
-                                                        </div>
-                                                        <div class="col-3">
-                                                            <label for="option_vl_price">Giá</label>
-                                                            <input type="text" class="form-control" id="option_vl_price" name="option_vl_price[]" placeholder="Giá trị">
-                                                        </div>
-                                                        <div class="col-1">
-                                                            <label for="">&nbsp;</label>
-                                                            <a href="javascript:void(0)" onclick="delete_(this)" class="btn btn-danger btn-sm d-block">Xóa</a>
-                                                        </div>
-                                                    </div>
-                                                `);
->>>>>>> b77ae48 ([admin] form add product variant)
       }
 
       function delete_(__this) {
